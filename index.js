53716b0b0689c0363704188344981679838f228c
anonymousss-112 <91783392+anonymousss-112@users.noreply.github.com>
2021-11-24 16:03:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,219 +1,93 @@
-let CalcBtn = document.getElementById('CalcBtn');
-CalcBtn.addEventListener('click' , getUnknown);
 
-let Pressure = document.getElementById('Pressure');
-let Volume = document.getElementById('Volume');
-let Moles = document.getElementById('Moles');
-let Temp = document.getElementById('Temp');
-let finalResult = document.getElementById('finalResult');
-let p,v,n,t;
-let R = 8.314;
-let calculatedValue;
-
-// disables the input for unknown value
-var radios = document.querySelectorAll('input[type=radio][name="solvefor"]');
-radios.forEach(radio => radio.addEventListener('change', () => {
-    if(radio.value=='pressure'){
-        Pressure.disabled=true;
-        Volume.disabled=false;
-        Moles.disabled=false;
-        Temp.disabled=false;
-        document.getElementById('pa').disabled=true;
-        document.getElementById('atm').disabled=true;
-        document.getElementById('bar').disabled=true;
-        document.getElementById('mmhg').disabled=true;
-
-        document.getElementById('l').disabled=false;
-        document.getElementById('m').disabled=false;
-        document.getElementById('cm').disabled=false;
-        document.getElementById('ft').disabled=false;
-
-        document.getElementById('C').disabled=false;
-        document.getElementById('K').disabled=false;
-        document.getElementById('F').disabled=false;
-        document.getElementById('R').disabled=false;
-    }
-    else if(radio.value=='volume'){
-        Volume.disabled=true;
-        Temp.disabled=false;
-        Moles.disabled=false;
-        Pressure.disabled=false;
-        document.getElementById('l').disabled=true;
-        document.getElementById('m').disabled=true;
-        document.getElementById('cm').disabled=true;
-        document.getElementById('ft').disabled=true;
-
-        document.getElementById('C').disabled=false;
-        document.getElementById('K').disabled=false;
-        document.getElementById('F').disabled=false;
-        document.getElementById('R').disabled=false;
-
-        document.getElementById('pa').disabled=false;
-        document.getElementById('atm').disabled=false;
-        document.getElementById('bar').disabled=false;
-        document.getElementById('mmhg').disabled=false;
-    }
-    else if(radio.value=='moles'){
-        Moles.disabled=true;
-        Temp.disabled=false;
-        Pressure.disabled=false;
-        Volume.disabled=false;
-
-        document.getElementById('C').disabled=false;
-        document.getElementById('K').disabled=false;
-        document.getElementById('F').disabled=false;
-        document.getElementById('R').disabled=false;
-
-        document.getElementById('pa').disabled=false;
-        document.getElementById('atm').disabled=false;
-        document.getElementById('bar').disabled=false;
-        document.getElementById('mmhg').disabled=false;
-
-        document.getElementById('l').disabled=false;
-        document.getElementById('m').disabled=false;
-        document.getElementById('cm').disabled=false;
-        document.getElementById('ft').disabled=false;
-    }
-    else if(radio.value=='temperature'){
-        Temp.disabled=true;
-        Moles.disabled=false;
-        Pressure.disabled=false;
-        Volume.disabled=false;
-        document.getElementById('C').disabled=true;
-        document.getElementById('K').disabled=true;
-        document.getElementById('F').disabled=true;
-        document.getElementById('R').disabled=true;
-
-        document.getElementById('pa').disabled=false;
-        document.getElementById('atm').disabled=false;
-        document.getElementById('bar').disabled=false;
-        document.getElementById('mmhg').disabled=false;
-
-        document.getElementById('l').disabled=false;
-        document.getElementById('m').disabled=false;
-        document.getElementById('cm').disabled=false;
-        document.getElementById('ft').disabled=false;
-    }
-}));
-
-//select unknown values 
-function getUnknown(){
-    if(document.getElementById('P').checked==true){
-        if(Volume.value=="" || Temp.value=="" || Moles.value==""){
-            alert("Please give proper input!");
-        }
-        else{
-            v=Vvalue();
-            t=Tvalue();
-            n=Mvalue();
-            calculatedValue = (n*R*t)/v;
-            finalResult.innerHTML = `<h3>Calculated value for pressure is : ${calculatedValue} in si units</h3>`
-        }
-    }
-    else if(document.getElementById('V').checked==true){
-        if(Pressure.value=="" || Temp.value=="" || Moles.value==""){
-            alert("Please give proper input!");
-        }
-        else{
-            p=Pvalue();
-            t=Tvalue();
-            n=Mvalue();
-            calculatedValue = (n*R*t)/p;
-            finalResult.innerHTML = `<h3>Calculated value for volume is : ${calculatedValue} in si units</h3>`
-        }
-    }
-    else if(document.getElementById('M').checked==true){
-        if(Volume.value=="" || Temp.value=="" || Pressure.value==""){
-            alert("Please give proper input!");
-        }
-        else{
-            v=Vvalue();
-            t=Tvalue();
-            p=Pvalue();
-            calculatedValue = (p*v)/R*t;
-            finalResult.innerHTML = `<h3>Calculated value for moles is : ${calculatedValue}</h3>`
-        }
-    }
-    else if(document.getElementById('T').checked==true){
-        if(Volume.value=="" || Pressure.value=="" || Moles.value==""){
-            alert("Please give proper input!");
-        }
-        else{
-            v=Vvalue();
-            p=Pvalue();
-            n=Mvalue();
-            calculatedValue = (p*v)/R*n;
-            finalResult.innerHTML = `<h3>Calculated value for temperature is : ${calculatedValue} in si units</h3>`
-        }
-    }
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
 }
 
-// returns value of pressure
-function Pvalue(){
-    p=Number(Pressure.value);
-    if(document.getElementById('pa').checked==true){
-        p=p;
-    }
-    else if(document.getElementById('atm').checked==true){
-        p=p*101325;
-    }
-    else if(document.getElementById('bar').checked==true){
-        p=p*100000;
-    }
-    else if(document.getElementById('mmhg').checked==true){
-        p=p*133.322;
-    }
-
-    return p;
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
 }
 
-//returns value of volume 
-function Vvalue(){
-    v=Number(Volume.value);
-    if(document.getElementById('m').checked==true){
-        v=v;
-    }
-    else if(document.getElementById('l').checked==true){
-        v=v*0.0001;
-    }
-    else if(document.getElementById('cm').checked==true){
-        v=v*0.000001;
-    }
-    else if(document.getElementById('ft').checked==true){
-        v=v*0.0283;
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
 
-    return v;
+    this.questionIndex++;
 }
 
-// retruns value of temperature
-function Tvalue(){
-    t=Number(Temp.value);
-    if(document.getElementById('K').checked==true){
-        t=t;
-    }
-    else if(document.getElementById('C').checked==true){
-        t=t+273.15;
-    }
-    else if(document.getElementById('F').checked==true){
-        t=(t-32)*0.56+273.15;
-    }
-    else if(document.getElementById('R').checked==true){
-       t=t*0.56;
-    }
-
-    return t;
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
 }
 
-// retruns value of moles
-function Mvalue(){
-    n=Number(Moles.value);
-    return n;
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
 }
 
-// clear button
-let ClrBtn = document.getElementById('ClrBtn');
-ClrBtn.addEventListener('click' , ()=>{
-    finalResult.innerHTML="";
-})
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
 
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+    new Question("Which of the following term does not involve in ideal gas law?", ["Pressure", "Volume","Temperature", "Time"], "Time"),
+    new Question("What is the shape of a P-T curve for ideal gas?", ["Straight line", "Parabolic", "Hyperbolic", "Ellipse"], "Straight line"),
+    new Question("What is the Temperature of a 2 l/mol ideal gas with pressure 10 atm?", ["243.6K", "268.4K","289.1K", "313.5K"], "243.6K"),
+    new Question("What is the compressibility factor of an ideal gas?", ["One", "Zero", "Infinite", "Depends on P and V"], "One"),
+    new Question("In an ideal gas, if the gas constant R=0.082, what is the unit of R?", ["L.atm/K.mol", "J/K.mol", "J/K", "J/mol"], "L.atm/K.mol"),
+    new Question("First of the two ideal gases has twice molar volume as much as the second at same temperature, what is the ratio of their pressures?", ["4:1", "1:4", "2:1", "1:2"], "1:2")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
